893ef06be848dabbb9938530122298b982f1e38c
Melanie <mlpatt23@gmail.com>
2019-11-05 14:44:28 -0700

    Added styleguide styles

--- inc/enqueues.php
+++ inc/enqueues.php
@@ -18,6 +18,9 @@ if (!(is_admin() )) {
  * Link to all theme CSS files.
  */
 function prelude_theme_scripts() {
+  // Fonts
+  wp_enqueue_style('typekit', 'https://use.typekit.net/bdt2vwc.css', array(), THEME_VERSION );
+
   // CSS
   wp_enqueue_style('prelude-css', get_template_directory_uri() . '/assets/css/theme.min.css', array(), THEME_VERSION );
 
