c8a9128204038dd2f4aec774d0748570fa0cc3b2
Melanie <mlpatt23@gmail.com>
2019-11-08 10:18:00 -0700

    Created basic 404 page

--- 404.php
+++ 404.php
@@ -1,15 +1,28 @@
 <?php
-  /**
-   * The 404 Not Found template.
-   *
-   * Used when WordPress encounters an unknown URL.
-   */
-  get_header();
-?>
+/**
+ * The 404 Not Found template.
+ *
+ * Used when WordPress encounters an unknown URL.
+ *
+ * @package F1 Telos Tennis
+ * @author Factor1 Studios
+ * @since 0.0.1
+ */
 
-  <section class="error-404">
+get_header(); ?>
 
-  </section>
+<section class="error-404">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1 class="script script--green">404</h1>
 
-<?php
-  get_footer();
+        <h2>Page Not Found</h2>
+
+        <p>Please head back to the <a href="<?php echo esc_url(home_url()); ?>">home page</a> and try again.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
