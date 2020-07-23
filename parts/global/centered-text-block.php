57e3652618a40d92aac8d35f5883fe12216ee37b
Melanie Rhodes <melanie@Melanies-iMac.local>
2020-07-23 09:17:47 -0700

    Added button option to centered text block

--- parts/global/centered-text-block.php
+++ parts/global/centered-text-block.php
@@ -17,7 +17,11 @@ $isVideo = is_singular('community-video');
 
 // Centered Text Block Custom Fields
 $colSpan = $isSingle || $isVideo ? '10' : get_sub_field('centered_text_block_column_span');
-$content = get_sub_field('centered_text_block_content'); ?>
+$content = get_sub_field('centered_text_block_content');
+$btnToggle = $isSingle || $isVideo ? false : get_sub_field('centered_text_block_button_toggle');
+$btnAlign = get_sub_field('centered_text_block_button_alignment');
+$btn = get_sub_field('centered_text_block_button');
+$btnColor = get_sub_field('centered_text_block_button_color'); ?>
 
 <section class="centered-text-block">
   <div class="container">
@@ -36,6 +40,17 @@ $content = get_sub_field('centered_text_block_content'); ?>
 
           <div class="flex-video">
             <iframe src="<?php echo $link; ?>" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+          </div>
+
+        <?php endif;
+
+        // Optional button
+        if( $btnToggle && $btn ) : ?>
+
+          <div class="text-<?php echo $btnAlign; ?>">
+            <a href="<?php echo esc_url($btn['url']); ?>" class="button button--<?php echo $btnColor; ?>" role="link" title="<?php echo $btn['title']; ?>" target="<?php echo $btn['target']; ?>">
+              <?php echo $btn['title']; ?>
+            </a>
           </div>
 
         <?php endif; ?>
