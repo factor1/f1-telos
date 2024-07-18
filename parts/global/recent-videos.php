ed573e1459439b9f720f3c0ba937bbc5ae5fdbc0
Jorge Parra <jorgeparraandrade@gmail.com>
2024-07-18 11:33:28 -0400

    recent videos ppp bumped to 12

--- parts/global/recent-videos.php
+++ parts/global/recent-videos.php
@@ -19,7 +19,7 @@ $btn = get_sub_field('recent_videos_button');
 if( $type ) :
   $args = array(
     'post_type' => 'community-video',
-    'posts_per_page' => 6,
+    'posts_per_page' => 12,
     'tax_query' => array(
       array(
         'taxonomy' => 'video-type',
@@ -31,7 +31,7 @@ if( $type ) :
 else :
   $args = array(
     'post_type' => 'community-video',
-    'posts_per_page' => 6,
+    'posts_per_page' => 12,
   );
 endif;
 
